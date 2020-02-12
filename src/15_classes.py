d868701daaa04f774e6634610abb517f2e5d2c97
Danny <53716622+DannyManzietti@users.noreply.github.com>
2020-02-12 13:27:14 -0500

    cleaning up per lecture

--- src/15_classes.py
+++ src/15_classes.py
@@ -35,7 +35,7 @@ class Geocache(Waypoint):
         self.size = size
 
     def __str__(self):
-        return f"name={self.name}, difficulty={self.difficulty}, size={self.size}, lat={self.lat}, lon={self.lon}"
+        return f"{self.name}, difficulty:{self.difficulty}, size:{self.size}, {self.lat}, {self.lon}"
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
